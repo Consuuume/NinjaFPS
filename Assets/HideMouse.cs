23e053538366ea1a68a4b635368023633911e40b
Jon <jmb9974@gmail.com>
2015-04-03 10:30:04 -0400

    Hide Mouse

---
+++ Assets/HideMouse.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class HideMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Escape))
+	    {
+            Application.Quit();
+	    }
+	}
+}

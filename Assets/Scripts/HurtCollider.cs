23ae099f7a3928f4d76f8798b79923d2995420cf
Jon <jmb9974@gmail.com>
2015-04-07 23:15:45 -0400

    Respawn

---
+++ Assets/Scripts/HurtCollider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HurtCollider : MonoBehaviour
+{
+    
+    // Use this for initialization
+	void Start ()
+	{
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "HitCollider")
+        {
+            transform.parent.GetComponent<Ninja>().Respawn();
+        }
+    }
+}
---
+++ Assets/Scripts/Ninja.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ninja : MonoBehaviour
+{
+
+    public float RespawnTime = 2.0f;
+    public Vector2 initialPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    initialPosition = new Vector2(transform.position.x, transform.position.y);
+	}
+
+    public void Respawn()
+    {
+        transform.position = initialPosition;
+    }
+}
